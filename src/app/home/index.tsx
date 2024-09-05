5e77cf7c0f9f257061e89e49da32309bee0dfffa
fariasleticia <leticiafcassimiro@gmail.com>
2024-09-05 16:15:04 -0300

    Criação e estilização de barra de pesquisa

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,12 +1,18 @@
 import { View } from 'react-native'
-
+import { Feather } from '@expo/vector-icons'
 import { styles } from './styles'
+import { theme } from '@/themes'
+import { Input } from '@/app/components/input'
 
 export function Home() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-
+                <Input style={styles.input}>
+                    <Feather name='search' size={16} color={theme.colors.gray_300}></Feather>
+                    <Input.Field placeholder='Pesquisar pelo nome...'/>
+                    <Feather name='x' size={16} color={theme.colors.gray_300}></Feather>
+                </Input>
             </View>
         </View>
     )
