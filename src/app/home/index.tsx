0ba7c646e07dad7dbcdfebff06640d680cc8f6cf
fariasleticia <leticiafcassimiro@gmail.com>
2024-09-05 15:00:01 -0300

    Estilização de cabeçalho e barra de status

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -4,6 +4,10 @@ import { styles } from './styles'
 
 export function Home() {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <View style={styles.header}>
+
+            </View>
+        </View>
     )
 }
