3fbf8b3c6aef9e5f94f3e4c3d410c47bb38672d9
fariasleticia <leticiafcassimiro@gmail.com>
2024-09-06 13:25:50 -0300

    Criação do avatar

---
+++ src/app/components/contact/index.tsx
@@ -0,0 +1,10 @@
+import { Text, TouchableOpacity } from "react-native"
+import { styles } from './styles'
+import { Avatar } from '../avatar'
+
+export function Contact(){
+    return <TouchableOpacity>
+        <Avatar name='Pedroso'/>
+        <Text style={styles.name}>Pedro</Text>
+    </TouchableOpacity>
+}
