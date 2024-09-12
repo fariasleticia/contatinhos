7a35609cbfd6890ee4aaa103e4c1ed13d17304d1
fariasleticia <leticiafcassimiro@gmail.com>
2024-09-12 09:55:26 -0300

    Busca de contatos

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -21,7 +21,24 @@ export function Home() {
             const { status } = await Contacts.requestPermissionsAsync()
             if (status === Contacts.PermissionStatus.GRANTED){
                 const { data } = await Contacts.getContactsAsync()
-                console.log(data)
+                const list = data.map((contact) => ({
+                    id: contact.id ?? useId(),
+                    name: contact.name,
+                    image: contact.image,
+                })).reduce<SectionListDataProps[]>((acc: any, item) => {
+                    const firstLetter = item.name[0].toUpperCase()
+                    const existingEntry = acc.find((entry: SectionListDataProps) =>
+                   (entry.title === firstLetter))
+                    
+                    if(existingEntry){
+                        existingEntry.data.push(item)
+                    }else{
+                        acc.push({title: firstLetter, data: [item]})
+                    }
+
+                    return acc
+                }, [])
+                setContacts(list)
             }
         } catch(error) {
             console.log(error)
@@ -48,10 +65,7 @@ export function Home() {
                 sections={[{title: "R", data: [{id: "1", name: "Heloísa"}] }]}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                   <Contact contact={{
-                        name: item.name,
-                        image: require("@/assets/avatar.jpeg")
-                    }}/> 
+                   <Contact contact={item}/> 
                 )}
                 renderSectionHeader = {({ section }) => 
                     (<Text style={styles.section}>{section.title}</Text>)}
